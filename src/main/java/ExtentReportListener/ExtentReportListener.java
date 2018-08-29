1745524dd2bb5ffeaebae8b9f66e52723adee220
yogeshs <yogeshsgondhali@gmail.com>
2018-08-29 12:17:29 +0530

    repo

--- src/main/java/ExtentReportListener/ExtentReportListener.java
+++ src/main/java/ExtentReportListener/ExtentReportListener.java
@@ -6,26 +6,34 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.testng.*;
+import org.testng.IResultMap;
+import org.testng.ISuite;
+import org.testng.ISuiteResult;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.xml.XmlSuite;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.IReporter;
 import com.relevantcodes.extentreports.LogStatus;
 
-public class ExtentReportListener implements IReporter
+public abstract class ExtentReportListener implements IReporter
 {
 	private ExtentReports extent;
 
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
-			String outputDirectory) {
+			String outputDirectory)
+	{
 		extent = new ExtentReports(outputDirectory + File.separator
 				+ "Extent.html", true);
 
-		for (ISuite suite : suites) {
+		for (ISuite suite : suites)
+		{
 			Map<String, ISuiteResult> result = suite.getResults();
 
-			for (ISuiteResult r : result.values()) {
+			for (ISuiteResult r : result.values()) 
+			{
 				ITestContext context = r.getTestContext();
 
 				buildTestNodes(context.getPassedTests(), LogStatus.PASS);
@@ -38,11 +46,14 @@ public class ExtentReportListener implements IReporter
 		extent.close();
 	}
 
-	private void buildTestNodes(IResultMap tests, LogStatus status) {
+	private void buildTestNodes(IResultMap tests, LogStatus status)
+	{
 		ExtentTest test;
 
-		if (tests.size() > 0) {
-			for (ITestResult result : tests.getAllResults()) {
+		if (tests.size() > 0)
+		{
+			for (ITestResult result : tests.getAllResults()) 
+			{
 				test = extent.startTest(result.getMethod().getMethodName());
 
 				test.setStartedTime(getTime(result.getStartMillis()));
@@ -51,7 +62,8 @@ public class ExtentReportListener implements IReporter
 				for (String group : result.getMethod().getGroups())
 					test.assignCategory(group);
 
-				if (result.getThrowable() != null) {
+				if (result.getThrowable() != null)
+				{
 					test.log(status, result.getThrowable());
 				} else {
 					test.log(status, "Test " + status.toString().toLowerCase()
@@ -63,14 +75,15 @@ public class ExtentReportListener implements IReporter
 		}
 	}
 
-	private Date getTime(long millis) {
+	private Date getTime(long millis)
+	{
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(millis);
 		return calendar.getTime();
 	}
 
-	
-	
-	
-	
+
+
+
+
 }
